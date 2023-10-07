df52d134919cbe298a11bdac96653e76559acbcd
bloodofpride <antonio_maxwell@hotmail.com>
2023-10-07 20:34:11 -0300

    method save user created

--- src/main/java/com/picpaysimplificado/domain/user/User.java
+++ src/main/java/com/picpaysimplificado/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.picpaysimplificado.domain.user;
 
+import com.picpaysimplificado.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -33,4 +34,14 @@ public class User {
     private UserType userType;
 
     private BigDecimal balance;
+
+    public User(UserDTO dto) {
+        firstname = dto.getFirstname();
+        lastname = dto.getLastname();
+        cpfCnpj = dto.getCpfCnpj();
+        email = dto.getEmail();
+        password = dto.getPassword();
+        userType = dto.getUserType();
+        balance = dto.getBalance();
+    }
 }
--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -1,6 +1,7 @@
 package com.picpaysimplificado.services;
 
 import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.UserDTO;
 import com.picpaysimplificado.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +22,22 @@ public class UserService {
     public User findByDoc(String doc) throws Exception {
         return userRepository.findByCpfCnpj(doc)
                 .orElseThrow(Exception::new);
+    }
+
+    public User save(UserDTO userDTO) throws Exception {
+        validateUser(userDTO);
+        User newUser = new User(userDTO);
+        userRepository.save(newUser);
+        return newUser;
+    }
+
+    private void validateUser(UserDTO userDTO) throws Exception {
+        if(userRepository.findByCpfCnpj(userDTO.getCpfCnpj()).isPresent()){
+            throw new Exception("já existe um usuário cadastrado no sistema com o documento: " + userDTO.getCpfCnpj());
+        }
+
+        if(userRepository.findByEmail(userDTO.getEmail()).isPresent()){
+            throw new Exception("já existe um usuário cadastrado no sistema com o email: " + userDTO.getEmail());
+        }
     }
 }
