59d9ebf8ab14135c273772aab8ea79e2d898cc55
bloodofpride <antonio_maxwell@hotmail.com>
2023-11-03 00:00:01 -0300

    ObjectNotFoundException created

---
+++ src/main/java/com/picpaysimplificado/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.picpaysimplificado.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
